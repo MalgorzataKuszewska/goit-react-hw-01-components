0b903d17ddeed72927441c2aa5259c23f2b888dd
MalgorzataKuszewska <mkuszewska242@gmail.com>
2023-10-17 22:05:36 +0200

    Add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,11 @@
+import React from 'react';
+import Profile from 'components/profile/Profile.js';
+import Statistics from 'components/statistics/Statistics.js';
+import FriendList from 'components/friendList/FriendList.js';
+import user from 'data/user.json';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +15,18 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
     </div>
   );
 };
---
+++ src/components/friendList/FriendList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className="item">
+      <span className="status">{isOnline ? 'online' : 'offline'}</span>
+      <img className="avatar" src={avatar} alt="User avatar" width="48" />
+      <p className="name">{name}</p>
+    </li>
+  );
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul class="friend-list">
+      {friends.map((friend, index) => (
+        <FriendListItem
+          key={index}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default FriendList;
